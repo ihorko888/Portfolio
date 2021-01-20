16242816d438f749557a25576fc0c5fe06172a9c
Ihor <kolotyuk@gmail.com>
2021-01-20 23:52:34 +0200

    Added nav burger menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,21 @@
 $(function () {
 
+    let navToggle = $('#navToggle');   
+    let nav = $('#nav-transform');
+    let navlinks = ('#nav-transform a');
     
+    navToggle.on('click', function(event) {
+        event.preventDefault();
+    
+        $("body").toggleClass('show-nav');
+        $(this).toggleClass('active');
+        nav.toggleClass('show');
+    });
+    
+    $(window).on('resize', function () {
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+    });
 
 });
