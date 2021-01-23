b79d16f8c713142c127bea447413891068ccd359
Ihor <kolotyuk@gmail.com>
2021-01-23 22:31:23 +0200

    Full desktop site

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,7 +29,7 @@ $(function () {
                 jQuery('.skillbar-container').each(function(){
                     jQuery(this).find('.skill').animate({
                         width:jQuery(this).attr('data-percent')
-                    }, 2000);
+                    }, 1000);
                 });
             });
         }
@@ -37,6 +37,7 @@ $(function () {
     
     
     
+    //  console.log("JavaScript is amazing!");
      $(document).ready(function($) {
        function animateElements() {
          $('.progressbar').each(function() {
@@ -65,5 +66,100 @@ $(function () {
        animateElements();
        $(window).scroll(animateElements);
      });
+    // Modal window
+    $('[data-modal]').on('click', function (event) {
+        event.preventDefault();
+    
+        let modal = $(this).data('modal');
+    
+        $('body').addClass('no-scroll');
+        $(modal).addClass('show');
+    
+        setTimeout(function () {
+            $(modal).find('.modal__inner').css({
+                transform: 'scale(1)',
+                opacity: '1'
+            });
+        }, 100);
+    });
+    
+    $('[data-modal-close]').on('click', function (event) {
+        event.preventDefault();
+    
+        let modal = $(this).parents('.modal');
+    
+        modalClose(modal);
+    });
+    
+    $('.modal').on('click', function () {
+        let modal = $(this);
+    
+        modalClose(modal);
+    });
+    
+    $('.modal__inner').on('click', function (event) {
+        event.stopPropagation();
+    });
+    
+    function modalClose(modal) {
+    
+        modal.find('.modal__inner').css({
+            transform: 'scale(0.5)',
+            opacity: '0'
+        });
+    
+        setTimeout(function () {
+            $('body').removeClass('no-scroll');
+            modal.removeClass('show');
+        }, 200);
+    }
+    // Fixed menu scroll
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop()) {
+            $('#header').addClass('header-show');
+        } else {
+            $('#header').removeClass('header-show');
+        };
+    });
+    // Scroll to section
+    let header = $('#header');
+    let headerH = header.innerHeight();
+    
+    $('[data-scroll]').on('click', function(event) {
+        event.preventDefault();
+    
+        let scrollEl = $(this).data('scroll');
+        let scrollElPos = $(scrollEl).offset().top;
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+    
+        $('html, body').animate({
+            scrollTop: scrollElPos - headerH
+        }, 500);
+    });
+    // ScrollSpy
+    let scrollTop = $(window).scrollTop();
+    let windowH = $(window).height();
+    scrollSpy(scrollTop);
+    
+    $(window).on("scroll", function () {
+        scrollTop = $(this).scrollTop();
+        scrollSpy(scrollTop);
+    });
+    
+    function scrollSpy(scrollTop) {
+        $("[data-scrollspy]").each(function () {
+            let $this = $(this);
+            let sectionId = $this.data('scrollspy');
+            let sectionOffset = $this.offset().top;
+            sectionOffset = sectionOffset - (windowH * 0.33333);
+    
+            if (scrollTop >= sectionOffset) {
+                $('#nav [data-scroll]').removeClass('active');
+                $('#nav [data-scroll="' + sectionId + '"]').addClass('active');
+            }
+        });
+    }
 
 });
