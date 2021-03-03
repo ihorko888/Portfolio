dbfcc17c8eaf088f48ed37e3b8687c255020e91e
Ihor <kolotyuk@gmail.com>
2021-03-03 18:48:48 +0200

    corrected the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -190,5 +190,27 @@ $(function () {
       anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
     
     });
+    // Date
+    let year = new Date().getFullYear();
+    let year1 = new Date().getFullYear();
+    document.getElementById('year').innerHTML = year;
+    document.getElementById('year1').innerHTML = year1;
+    // Webp CSS
+    function testWebP(callback) {
+      var webP = new Image();
+      webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+      };
+      webP.src =
+        "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
+    
+    testWebP(function (support) {
+      if (support == true) {
+        document.querySelector("body").classList.add("webp");
+      } else {
+        document.querySelector("body").classList.add("no-webp");
+      }
+    });
 
 });
